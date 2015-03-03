a9f3e0bc1ee97e45ac6732330482fbee33696ebc
John Turner <j.turner@mhnltd.co.uk>
2015-03-03 11:13:07 +0000

    Basic shader loading from files.

---
+++ Makefile
@@ -0,0 +1,39 @@
+SOURCES=$(wildcard *.cpp)
+OBJECTS=$(SOURCES:.cpp=.o)
+
+BIN_DIR=bin
+DEP_DIR=deps
+BIN=$(BIN_DIR)/fractalview
+
+CPPC=clang++
+SHARED_ARGS=-std=gnu++0x -Ofast -g -ggdb -m64 -mmmx -msse -msse2 -m3dnow -fexceptions -Wall -Wpedantic -DDEBUG -DNO_SHOCKWAVE_DEBUG -DVSYNC
+COMPILE_ARGS=-c $(SHARED_ARGS)
+LINK_ARGS=$(SHARED_ARGS)
+LIBS=-lglfw -lGL -lGLEW
+INCLUDE=-I.
+
+$(BIN): $(OBJECTS) Makefile $(BIN_DIR)
+	$(CPPC) $(LINK_ARGS) $(OBJECTS) $(INCLUDE) $(LIBS) -o $(BIN)
+
+$(DEP_DIR):
+	mkdir -p $(DEP_DIR)
+
+$(BIN_DIR):
+	mkdir -p $(BIN_DIR)
+
+%.o: %.cpp Makefile $(DEP_DIR)
+	$(CPPC) $(COMPILE_ARGS) $< -MMD -MF $(DEP_DIR)/$(patsubst %.o,%.md,$@)
+
+run: $(BIN)
+	./$(BIN)
+
+debug: $(BIN)
+	gdb ./$(BIN)
+
+valg: $(BIN)
+	valgrind --leak-check=full $(BIN)
+
+include $(wildcard $(DEP_DIR)/*.md)
+
+clean:
+	rm $(BIN) $(OBJECTS); true
---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include "shader_load.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <vector>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+GLFWwindow *window;
+GLuint shader;
+GLuint vao;
+GLuint vbo;
+
+static double constexpr fpscap = 60.0;
+static double constexpr timestep = 1.0 / fpscap;     // assume we're running at a fixed 60fps
+std::chrono::duration<double> timestep_chrono(std::chrono::milliseconds(static_cast<unsigned int>(timestep * 1000) - 1));
+std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::duration<double>> timenexttickstart;
+
+void glfw_error(int error, const char* description) {
+  cerr << "GLFW Error: " << error << ": " << description << endl;
+}
+
+void load_shader() {
+  cout << "Loading shader." << endl;
+  
+  std::stringstream fragss;
+  fragss << std::ifstream("mandelbulb.frag").rdbuf();
+
+  std::stringstream vertss;
+  vertss << std::ifstream("mandelbulb.vert").rdbuf();
+
+  shader = shader_load(vertss.str(), fragss.str());
+  cout << "Loaded." << endl;
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glGenBuffers(1, &vbo);
+
+  GLfloat vertices[] = {
+      -1.0f, -1.0f,
+       1.0f, -1.0f,
+       1.0f,  1.0f,
+      -1.0f,  1.0f
+  };
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  GLuint position_attribute = glGetAttribLocation(shader, "position");
+  glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(position_attribute);
+
+  glUseProgram(shader);
+}
+
+void init_graphics() {
+  if (!glfwInit()) {
+    cout << "Failed to init glfw." << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  glfwSetErrorCallback(glfw_error);
+  window = glfwCreateWindow(800, 600, "touch the sky", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glfwSetWindowTitle(window, "touch the sky");
+  glfwShowWindow(window);
+
+#ifdef NO_VSYNC
+  glfwSwapInterval(0);
+#endif
+
+  glewInit(); 
+}
+
+int main() {
+  float fps = 0.0f;
+  float ms_per_frame = 0.0f;
+  double fpsinterval = 10.0;           // how many seconds between reporting performance
+  int frames_last_interval = 0;
+  std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::duration<double>> time_fpsupdate = std::chrono::high_resolution_clock::now() + std::chrono::duration<double>(std::chrono::milliseconds(static_cast<int>(1000 * fpsinterval)));
+
+  cout << "Touch sky." << endl;
+  init_graphics();
+
+  load_shader();
+  while(!glfwWindowShouldClose(window)) {
+    int window_width, window_height;
+    glfwGetWindowSize(window, &window_width, &window_height);
+
+    // Draw
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glDrawArrays(GL_QUADS, 0, 4);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    // fps counter update
+    std::chrono::time_point<std::chrono::high_resolution_clock, std::chrono::duration<double>> const time_now(std::chrono::high_resolution_clock::now());
+    // measure performance margin
+    if(time_now >= time_fpsupdate) {
+      fps = frames_last_interval / fpsinterval;
+      ms_per_frame = fpsinterval / frames_last_interval;
+      std::cout << "ms per frame: " << ms_per_frame << " fps: " << fps << std::endl;
+      time_fpsupdate = time_now + std::chrono::duration<double>(std::chrono::milliseconds(static_cast<int>(1000 * fpsinterval)));
+      frames_last_interval = 0;
+    } else {
+      ++frames_last_interval;
+    }
+
+    // Sleep until next update. (not needed if glfw is vsyncing)
+    //std::this_thread::sleep_until(timenexttickstart);
+    timenexttickstart = std::chrono::high_resolution_clock::now() + timestep_chrono;
+  }
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
+}
